e8a0365db1373bb4affbe36968b58b41cc96f2b7
Darien <63910112+Darien2805@users.noreply.github.com>
2022-07-29 01:47:26 +0800

    Addition of top button

--- js/profile.js
+++ js/profile.js
@@ -195,5 +195,21 @@ var arrowBounce = function() {
 // run the arrowBounce function every 800ms
 setInterval(arrowBounce, 800);
 
-// TEST
+//top button
+var topbtn = document.getElementById("topbtn");
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    topbtn.style.display = "block";
+  } else {
+    topbtn.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
 
